dec8b5750154f793b6f077cb208fd648d37f8c08
maratsak <96316593+maratsak@users.noreply.github.com>
2022-02-24 00:13:29 +0500

    Add files via upload

---
+++ tiktaktoe/index.js
@@ -0,0 +1,56 @@
+var area = document.getElementById('area') != null;
+var cell = document.getElementsByClassName('cell');
+var player = "x";
+var winIndex = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+for (var i = 1; i <= 9; i++) {
+    area.innerHTML += "<div class='cell' pos=" + i + "></div>";
+}
+for (var i = 0; i < cell.length; i++) {
+    cell[i].addEventListener('click', cellClick, false);
+}
+
+function cellClick() {
+    var data = [];
+    if (!this.innerHTML) {
+        this.innerHTML = player;
+    } else {
+        alert("Ячейка занята");
+        return;
+    }
+    for (var i in cell) {
+        if (cell[i].innerHTML == player) {
+            data.push(parseInt(cell[i].getAttribute('pos')));
+        }
+    }
+
+
+    if (checkWin(data)) {
+        alert("Выиграл: " + player);
+    }
+    player = player == "x" ? "o" : "x";
+    console.log(data);
+}
+
+function checkWin(data) {
+    for (var i in winIndex) {
+        var win = true;
+        for (var j in winIndex[i]) {
+            var id = winIndex[i][j];
+            var ind = data.indexOf(id);
+            if (win == -1) {
+                win = false
+            }
+        }
+        if (win) return true;
+    }
+    return false;
+}
